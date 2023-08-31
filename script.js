a46ce0bcd0b6759d4e5b99c1ec445090be90f364
Joaquin Talice <joaquintalice@gmail.com>
2023-08-31 00:37:58 -0300

    fix

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 document.addEventListener('DOMContentLoaded', init);
 
 const dataContainer = document.getElementById('data-container')
+const spinner = document.querySelector('.spinner-container');
 
 
 function init() {
@@ -13,7 +14,12 @@ function filter() {
     const btnSearch = document.getElementById('btn-search');
 
     btnSearch.addEventListener('click', () => {
-        localStorage.clear('item')
+        if (inputSearch.value < 1) {
+            localStorage.removeItem('item')
+            spinner.style.display = 'none';
+            return;
+        }
+        localStorage.removeItem('item')
         localStorage.setItem('item', inputSearch.value)
         location.reload()
     })
@@ -32,7 +38,6 @@ async function getData() {
     const { collection } = data
 
     const slicedData = collection.items.slice(0, 21);
-    // console.log(slicedData)
 
     showData(slicedData)
 
@@ -42,64 +47,58 @@ async function getImages(url) {
     const response = await fetch(url, { contentType: 'application/json', dataType: 'jsonp', });
     if (!response.ok) throw new Error(`Error getting images. Code error ${response.status}`)
     const data = await response.json();
-    // console.log(data)
+
     return data
 }
 
 async function showData(dataObject) {
 
-
     let template = ``;
 
-    // console.log(dataObject)
     for (let item of dataObject) {
 
         const { data, href } = item
-        const dataItem = data[0];
-        // console.log(dataItem)
 
-        // console.log(item.href)
+        const dataItem = data[0];
+
         const images = await getImages(href)
+
         if (images[0].endsWith(".jpg")) {
 
             template += `
-    <div class="col-12 col-md-6 col-xl-4">
-        <div class="card">
-            <img src="${images[0]}" class="card-img-top" height='400' alt="...">
-            <div class="card-body">
-                <h5 class="card-title">${dataItem.title}</h5>
-                <div class="overflow-auto" style="height:150px;">
-                    <p class="card-text">${dataItem.description}</p>
+                <div class="col-12 col-md-6 col-xl-4">
+                    <div class="card">
+                        <img src="${images[0]}" class="card-img-top" height='400' alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">${dataItem.title}</h5>
+                            <div class="overflow-auto" style="height:150px;">
+                                <p class="card-text">${dataItem.description}</p>
+                            </div>
+                            
+                        </div>
+                    </div>
                 </div>
-                
-            </div>
-        </div>
-    </div>
         `
         } else if (images[0].endsWith(".mp4")) {
             template += `
 
             <div class="col-12 col-md-6 col-xl-4">
-        <div class="card">
-        <div class="embed-responsive embed-responsive-1by1  text-center">
-        <iframe class="embed-responsive-item manual-responsive" src="${images[0]}" allowfullscreen></iframe>
-    </div>
-            <div class="card-body">
-                <h5 class="card-title">${dataItem.title}</h5>
-                <div class="overflow-auto" style="height:150px;">
-                    <p class="card-text">${dataItem.description}</p>
+                <div class="card">
+                    <div class="embed-responsive embed-responsive-1by1  text-center">
+                        <iframe class="embed-responsive-item manual-responsive" src="${images[0]}" allowfullscreen></iframe>
+                    </div>
+                    <div class="card-body">
+                        <h5 class="card-title">${dataItem.title}</h5>
+                        <div class="overflow-auto" style="height:150px;">
+                            <p class="card-text">${dataItem.description}</p>
+                        </div>
+                    </div>
                 </div>
-                
             </div>
-        </div>
-    </div>
-
-            
             `
         }
     }
-
     dataContainer.innerHTML = template
-    document.querySelector('.spinner-container').style.display = 'none';
+    spinner.style.display = 'none';
 
 }
